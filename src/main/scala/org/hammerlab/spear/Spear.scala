76e38e45edf673c3b13f98128657577900a588b8
Ryan Williams <ryan.blake.williams@gmail.com>
2015-05-06 04:19:43 +0000

    finish implementing SparkListener interface

--- src/main/scala/org/hammerlab/spear/Spear.scala
+++ src/main/scala/org/hammerlab/spear/Spear.scala
@@ -1,12 +1,15 @@
 
 package org.hammerlab.spear
 
-import com.mongodb.casbah.MongoClient
+import java.util.Properties
+import com.mongodb.casbah.{MongoCollection, MongoClient}
 import com.novus.salat._
 import com.novus.salat.global._
+import scala.collection.JavaConversions._
 
 import org.apache.spark.{
     TaskEndReason => SparkTaskEndReason,
+    SparkContext,
     Success,
     UnknownReason,
     TaskKilled,
@@ -28,11 +31,13 @@ import org.apache.spark.storage.{
     RDDInfo => SparkRDDInfo,
     StorageLevel => SparkStorageLevel,
     BlockStatus => SparkBlockStatus,
+    BlockManagerId => SparkBlockManagerId,
     BlockId
 }
 import org.apache.spark.scheduler.{
     StageInfo => SparkStageInfo,
     TaskInfo => SparkTaskInfo,
+    JobSucceeded,
     SparkListenerExecutorRemoved,
     SparkListenerExecutorAdded,
     SparkListenerExecutorMetricsUpdate,
@@ -51,6 +56,7 @@ import org.apache.spark.scheduler.{
     SparkListenerTaskEnd,
     SparkListenerTaskStart
 }
+import org.apache.spark.scheduler.cluster.{ExecutorInfo => SparkExecutorInfo}
 
 
 case class TaskInfo(taskId: Long,
@@ -64,7 +70,16 @@ case class TaskInfo(taskId: Long,
 
 object TaskInfo {
   def apply(t: SparkTaskInfo): TaskInfo =
-    new TaskInfo(t.taskId, t.index, t.attempt, t.launchTime, t.executorId, t.host, t.taskLocality.id, t.speculative)
+    new TaskInfo(
+      t.taskId,
+      t.index,
+      t.attempt,
+      t.launchTime,
+      t.executorId,
+      t.host,
+      t.taskLocality.id,
+      t.speculative
+    )
 }
 
 case class StorageLevel(n: Int)
@@ -97,7 +112,14 @@ case class StageInfo(stageId: Int,
 
 object StageInfo {
   def apply(s: SparkStageInfo): StageInfo =
-    new StageInfo(s.stageId, s.attemptId, s.name, s.numTasks, s.rddInfos.map(RDDInfo.apply), s.details)
+    new StageInfo(
+      s.stageId,
+      s.attemptId,
+      s.name,
+      s.numTasks,
+      s.rddInfos.map(RDDInfo.apply),
+      s.details
+    )
 }
 
 case class InputMetrics(bytesRead: Long, recordsRead: Long)
@@ -107,7 +129,11 @@ object InputMetrics {
 
 case class OutputMetrics(bytesWritten: Long, recordsWritten: Long)
 object OutputMetrics {
-  def apply(o: SparkOutputMetrics): OutputMetrics = new OutputMetrics(o.bytesWritten, o.recordsWritten)
+  def apply(o: SparkOutputMetrics): OutputMetrics =
+    new OutputMetrics(
+      o.bytesWritten,
+      o.recordsWritten
+    )
 }
 
 case class ShuffleReadMetrics(remoteBlocksFetched: Int,
@@ -131,7 +157,11 @@ object ShuffleReadMetrics {
 case class ShuffleWriteMetrics(shuffleBytesWritten: Long, shuffleWriteTime: Long, shuffleRecordsWritten: Long)
 object ShuffleWriteMetrics {
   def apply(s: SparkShuffleWriteMetrics): ShuffleWriteMetrics =
-    new ShuffleWriteMetrics(s.shuffleBytesWritten, s.shuffleWriteTime, s.shuffleRecordsWritten)
+    new ShuffleWriteMetrics(
+      s.shuffleBytesWritten,
+      s.shuffleWriteTime,
+      s.shuffleRecordsWritten
+    )
 }
 
 case class BlockStatus(storageLevel: StorageLevel,
@@ -186,14 +216,18 @@ case class TaskStartEvent(stageId: Int, stageAttemptId: Int, taskInfo: TaskInfo)
 
 object TaskStartEvent {
   def apply(s: SparkListenerTaskStart): TaskStartEvent =
-    new TaskStartEvent(s.stageId, s.stageAttemptId, TaskInfo(s.taskInfo))
+    new TaskStartEvent(
+      s.stageId,
+      s.stageAttemptId,
+      TaskInfo(s.taskInfo)
+    )
 }
 
-case class TaskEndReason(success: Boolean = false,
-                         resubmitted: Boolean = false,
-                         taskResultLost: Boolean = false,
-                         taskKilled: Boolean = false,
-                         unknownReason: Boolean = false,
+case class TaskEndReason(success: Option[Boolean] = None,
+                         resubmitted: Option[Boolean] = None,
+                         taskResultLost: Option[Boolean] = None,
+                         taskKilled: Option[Boolean] = None,
+                         unknownReason: Option[Boolean] = None,
                          fetchFailed: Option[FetchFailed] = None,
                          exceptionFailure: Option[ExceptionFailure] = None,
                          taskCommitDenied: Option[TaskCommitDenied] = None,
@@ -201,11 +235,11 @@ case class TaskEndReason(success: Boolean = false,
 
 object TaskEndReason {
   def apply(r: SparkTaskEndReason): TaskEndReason = r match {
-    case Success => TaskEndReason(success = true)
-    case Resubmitted => TaskEndReason(resubmitted = true)
-    case TaskResultLost => TaskEndReason(taskResultLost = true)
-    case TaskKilled => TaskEndReason(taskKilled = true)
-    case UnknownReason => TaskEndReason(unknownReason = true)
+    case Success => TaskEndReason(success = Some(true))
+    case Resubmitted => TaskEndReason(resubmitted = Some(true))
+    case TaskResultLost => TaskEndReason(taskResultLost = Some(true))
+    case TaskKilled => TaskEndReason(taskKilled = Some(true))
+    case UnknownReason => TaskEndReason(unknownReason = Some(true))
     case e: FetchFailed => TaskEndReason(fetchFailed = Some(e))
     case e: ExceptionFailure => TaskEndReason(exceptionFailure = Some(e))
     case e: TaskCommitDenied => TaskEndReason(taskCommitDenied = Some(e))
@@ -232,64 +266,206 @@ object TaskEndEvent {
     )
 }
 
-class Spear extends SparkListener {
+object Props {
+  type Props = Map[String, String]
+  def apply(properties: Properties): Props =
+    properties.stringPropertyNames().map(name => name -> properties.getProperty(name)).toMap
+}
+
+import Props.Props
+
+case class JobStartEvent(jobId: Int,
+                         time: Long,
+                         stageInfos: Seq[StageInfo],
+                         properties: Option[Props] = None)
+object JobStartEvent {
+  def apply(e: SparkListenerJobStart): JobStartEvent =
+    new JobStartEvent(
+      e.jobId,
+      e.time,
+      e.stageInfos.map(StageInfo.apply),
+      Option(e.properties).map(Props.apply)
+    )
+}
+
+
+case class JobEndEvent(jobId: Int,
+                       time: Long,
+                       success: Boolean)
+object JobEndEvent {
+  def apply(e: SparkListenerJobEnd): JobEndEvent =
+    JobEndEvent(
+      e.jobId,
+      e.time,
+      e.jobResult match {
+        case JobSucceeded => true
+        case _ => false
+      }
+    )
+}
+
+case class BlockManagerId(executorId: String, host: String, port: Int)
+object BlockManagerId {
+  def apply(b: SparkBlockManagerId): BlockManagerId =
+    BlockManagerId(
+      b.executorId,
+      b.host,
+      b.port
+    )
+}
+
+case class BlockManagerAddedEvent(time: Long, blockManagerId: BlockManagerId, maxMem: Long)
+object BlockManagerAddedEvent {
+  def apply(e: SparkListenerBlockManagerAdded): BlockManagerAddedEvent =
+    BlockManagerAddedEvent(
+      e.time,
+      BlockManagerId(e.blockManagerId),
+      e.maxMem
+    )
+}
+
+case class BlockManagerRemovedEvent(time: Long, blockManagerId: BlockManagerId)
+object BlockManagerRemovedEvent {
+  def apply(e: SparkListenerBlockManagerRemoved): BlockManagerRemovedEvent =
+    BlockManagerRemovedEvent(
+      e.time,
+      BlockManagerId(e.blockManagerId)
+    )
+}
+
+case class ExecutorInfo(executorHost: String,
+                        totalCores: Int,
+                        logUrlMap: Map[String, String])
+object ExecutorInfo {
+  def apply(e: SparkExecutorInfo): ExecutorInfo =
+    ExecutorInfo(
+      e.executorHost,
+      e.totalCores,
+      e.logUrlMap
+    )
+}
+
+case class ExecutorAddedEvent(time: Long, executorId: String, executorInfo: ExecutorInfo)
+object ExecutorAddedEvent {
+  def apply(e: SparkListenerExecutorAdded): ExecutorAddedEvent =
+    ExecutorAddedEvent(
+      e.time,
+      e.executorId,
+      ExecutorInfo(e.executorInfo)
+    )
+}
+
+case class ExecutorMetricsUpdateEvent(execId: String,
+                                      taskMetrics: Seq[(Long, Int, Int, TaskMetrics)])
+object ExecutorMetricsUpdateEvent {
+  def apply(e: SparkListenerExecutorMetricsUpdate): ExecutorMetricsUpdateEvent =
+    ExecutorMetricsUpdateEvent(
+      e.execId,
+      e.taskMetrics.map(p => (p._1, p._2, p._3, TaskMetrics(p._4)))
+    )
+}
+
+class Spear(sc: SparkContext) extends SparkListener {
+
+  sc.addSparkListener(this)
+  val applicationId = sc.applicationId
 
   val client = MongoClient("localhost", 27017)
-  val db = client("spark")
-  val stages = db("stages")
-  val completedStages = db("completed_stages")
+  val db = client(applicationId)
+
+
+  // Collections
+  val submittedStages = db("stage_starts")
+  val completedStages = db("stage_ends")
+
+  val startedTasks = db("task_starts")
+  val taskGettingResults = db("task_getting_results")
+  val endedTasks = db("task_ends")
+
+  val jobStarts = db("job_starts")
+  val jobEnds = db("job_ends")
+
+  val envUpdates = db("env_updates")
+
+  val blockManagerAdds = db("block_manager_adds")
+  val blockManagerRemoves = db("block_manager_removes")
+
+  val rddUnpersists = db("rdd_unpersists")
+
+  val appStarts = db("app_starts")
+  val appEnds = db("app_ends")
+
+  val executorMetricUpdates = db("executor_updates")
+  val executorAdds = db("executor_adds")
+  val executorRemoves = db("executor_removes")
+
+
+  def serializeAndInsert[T <: AnyRef](t: T, collection: MongoCollection)(implicit m: Manifest[T]): Unit = {
+    val dbo = grater[T].asDBObject(t)
+    collection.insert(dbo)
+  }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
-    val dbo = grater[StageInfo].asDBObject(StageInfo(stageCompleted.stageInfo))
-    completedStages.insert(dbo)
+    serializeAndInsert(StageInfo(stageCompleted.stageInfo), completedStages)
   }
 
   override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted): Unit = {
-    val dbo = grater[StageInfo].asDBObject(StageInfo(stageSubmitted.stageInfo))
-    stages.insert(dbo)
+    serializeAndInsert(StageInfo(stageSubmitted.stageInfo), submittedStages)
   }
 
   override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
-
+    serializeAndInsert(TaskStartEvent(taskStart), startedTasks)
   }
 
   override def onTaskGettingResult(taskGettingResult: SparkListenerTaskGettingResult): Unit = {
-
+    serializeAndInsert(TaskInfo(taskGettingResult.taskInfo), taskGettingResults)
   }
 
   override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
-
+    serializeAndInsert(TaskEndEvent(taskEnd), endedTasks)
   }
 
   override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
-
+    serializeAndInsert(JobStartEvent(jobStart), jobStarts)
   }
 
   override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
-
+    serializeAndInsert(JobEndEvent(jobEnd), jobEnds)
   }
 
-  override def onEnvironmentUpdate(environmentUpdate: SparkListenerEnvironmentUpdate): Unit = {}
+  override def onEnvironmentUpdate(environmentUpdate: SparkListenerEnvironmentUpdate): Unit = {
+    serializeAndInsert(environmentUpdate, envUpdates)
+  }
 
-  override def onBlockManagerAdded(blockManagerAdded: SparkListenerBlockManagerAdded): Unit = {}
+  override def onBlockManagerAdded(blockManagerAdded: SparkListenerBlockManagerAdded): Unit = {
+    serializeAndInsert(BlockManagerAddedEvent(blockManagerAdded), blockManagerAdds)
+  }
 
-  override def onBlockManagerRemoved(blockManagerRemoved: SparkListenerBlockManagerRemoved): Unit = {}
+  override def onBlockManagerRemoved(blockManagerRemoved: SparkListenerBlockManagerRemoved): Unit = {
+    serializeAndInsert(BlockManagerRemovedEvent(blockManagerRemoved), blockManagerRemoves)
+  }
 
-  override def onUnpersistRDD(unpersistRDD: SparkListenerUnpersistRDD): Unit = {}
+  override def onUnpersistRDD(unpersistRDD: SparkListenerUnpersistRDD): Unit = {
+    serializeAndInsert(unpersistRDD, rddUnpersists)
+  }
 
-  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {}
+  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
+    serializeAndInsert(applicationStart, appStarts)
+  }
 
-  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {}
+  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {
+    serializeAndInsert(applicationEnd, appEnds)
+  }
 
   override def onExecutorMetricsUpdate(executorMetricsUpdate: SparkListenerExecutorMetricsUpdate): Unit = {
-
+    serializeAndInsert(ExecutorMetricsUpdateEvent(executorMetricsUpdate), executorMetricUpdates)
   }
 
   override def onExecutorAdded(executorAdded: SparkListenerExecutorAdded): Unit = {
-
+    serializeAndInsert(ExecutorAddedEvent(executorAdded), executorAdds)
   }
 
   override def onExecutorRemoved(executorRemoved: SparkListenerExecutorRemoved): Unit = {
-
+    serializeAndInsert(executorRemoved, executorRemoves)
   }
 }
