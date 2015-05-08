c30770b045aa3db051957ec46790f77af94d3400
Ryan Williams <ryan.blake.williams@gmail.com>
2015-05-08 17:39:26 +0000

    better test mongo configuration

--- src/test/scala/org/hammerlab/spear/MongoTestCollection.scala
+++ src/test/scala/org/hammerlab/spear/MongoTestCollection.scala
@@ -4,19 +4,28 @@ import com.github.fakemongo.Fongo
 import com.mongodb.casbah.{MongoClient, MongoCollection}
 
 trait MongoTestCollection {
+
+  def getMongoConfig: Option[(String, Int)] = {
+    val mongoOpt = Option(System.getProperty("mongo"))
+    val hostOpt = Option(System.getProperty("mongo.host")).orElse(Option(System.getProperty("mongo.hostname")))
+    val portOpt = Option(System.getProperty("mongo.port")).map(_.toInt)
+    mongoOpt.orElse(hostOpt).orElse(portOpt).map(_ => (hostOpt.getOrElse("localhost"), portOpt.getOrElse(27017)))
+  }
+
   val collection: MongoCollection =
-    if (System.getProperty("mongo") != null) {
-      println(s"using real mongo... ${System.getProperty("mongo")}")
-      val client = MongoClient("localhost", 27017)
-      val db = client("test")
-      val collection = db("test")
-      collection.drop()
-      collection
-    } else {
-      println(s"using fake mongo...")
-      val mongo = new Fongo("fake-mongo")
-      val db = mongo.getDB("test-db")
-      val collection = db.getCollection("test-collection")
-      new MongoCollection(collection)
+    getMongoConfig match {
+      case Some((hostname, port)) =>
+        println(s"using real mongo at ${hostname}:${port}")
+        val client = MongoClient(hostname, port)
+        val db = client("test")
+        val collection = db("test")
+        collection.drop()
+        collection
+      case _ =>
+        println(s"using 'fake' in-memory mongo")
+        val mongo = new Fongo("fake-mongo")
+        val db = mongo.getDB("test-db")
+        val collection = db.getCollection("test-collection")
+        new MongoCollection(collection)
     }
 }
