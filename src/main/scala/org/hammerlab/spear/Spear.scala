eff1ea7b0a3777b3784fc1f6be350a2e4c90ab49
Ryan Williams <ryan.blake.williams@gmail.com>
2015-05-14 14:47:55 +0000

    move Job events to top of Spear class

--- src/main/scala/org/hammerlab/spear/Spear.scala
+++ src/main/scala/org/hammerlab/spear/Spear.scala
@@ -60,6 +60,54 @@ class Spear(sc: SparkContext,
     )
   )
 
+  // Job events
+  override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
+
+    val job = Job.newBuilder
+              .id(jobStart.jobId)
+              .startTime(jobStart.time)
+              .stageIDs(jobStart.stageIds)
+              .properties(SparkIDL.properties(jobStart.properties))
+              .result()
+    db.insert(job)
+
+    val stages = jobStart.stageInfos.map(si => {
+      Stage.newBuilder
+      .id(si.stageId)
+      .attempt(si.attemptId)
+      .name(si.name)
+      .numTasks(si.numTasks)
+      .rddIDs(si.rddInfos.map(_.id))
+      .details(si.details)
+      .startTime(si.submissionTime)
+      .result()
+    })
+
+    // NOTE(ryan): assumes that this happens before the StageSubmitted event,
+    // otherwise we'd get duplicate Stage records.
+    db.insertAll(stages)
+
+    val rdds = jobStart.stageInfos.flatMap(_.rddInfos).map(ri => {
+      RDD.newBuilder
+      .id(ri.id)
+      .name(ri.name)
+      .numPartitions(ri.numPartitions)
+      .storageLevel(SparkIDL.storageLevel(ri.storageLevel))
+      .result()
+    })
+
+    db.insertAll(rdds)
+  }
+
+  override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
+    db.findAndUpdateOne(
+      Q(Job)
+      .where(_.id eqs jobEnd.jobId)
+      .findAndModify(_.endTime setTo jobEnd.time)
+      .and(_.succeeded setTo (jobEnd.jobResult == JobSucceeded))
+    )
+  }
+
   // Stage events
   override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted): Unit = {
     val si = stageSubmitted.stageInfo
@@ -191,54 +239,6 @@ class Spear(sc: SparkContext,
     val metricsDeltas = getTaskMetricsDeltasMap(metricsUpdates)
     updateStageMetrics(metricsUpdates, metricsDeltas)
     updateExecutorMetrics(ti.executorId, metricsUpdates, metricsDeltas)
-  }
-
-  // Job events
-  override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
-
-    val job = Job.newBuilder
-              .id(jobStart.jobId)
-              .startTime(jobStart.time)
-              .stageIDs(jobStart.stageIds)
-              .properties(SparkIDL.properties(jobStart.properties))
-              .result()
-    db.insert(job)
-
-    val stages = jobStart.stageInfos.map(si => {
-      Stage.newBuilder
-        .id(si.stageId)
-        .attempt(si.attemptId)
-        .name(si.name)
-        .numTasks(si.numTasks)
-        .rddIDs(si.rddInfos.map(_.id))
-        .details(si.details)
-        .startTime(si.submissionTime)
-        .result()
-    })
-
-    // NOTE(ryan): assumes that this happens before the StageSubmitted event,
-    // otherwise we'd get duplicate Stage records.
-    db.insertAll(stages)
-
-    val rdds = jobStart.stageInfos.flatMap(_.rddInfos).map(ri => {
-      RDD.newBuilder
-        .id(ri.id)
-        .name(ri.name)
-        .numPartitions(ri.numPartitions)
-        .storageLevel(SparkIDL.storageLevel(ri.storageLevel))
-        .result()
-    })
-
-    db.insertAll(rdds)
-  }
-
-  override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
-    db.findAndUpdateOne(
-      Q(Job)
-        .where(_.id eqs jobEnd.jobId)
-        .findAndModify(_.endTime setTo jobEnd.time)
-        .and(_.succeeded setTo (jobEnd.jobResult == JobSucceeded))
-    )
   }
 
   def getTaskMetricsDeltasMap(metrics: Seq[(TaskID, _, _, SparkTaskMetrics)]): Map[TaskID, TaskMetrics] = {
